2fb539be0b1705383600e9d1d42dc27c8a053cb6
Shumei Huang <rose1264@gmail.com>
2018-06-14 21:46:52 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,4 +1,6 @@
+ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
   arr.sort_by { |string| string.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
